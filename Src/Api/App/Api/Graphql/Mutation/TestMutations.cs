101edb556381b68650f284722bf216bcf84e023d
naufil.shaikh <naufil.shaikh@webosphere.in>
2021-02-07 13:32:56 +0530

    Added Graphql with Hot chocolate.

---
+++ Src/Api/App/Api/Graphql/Mutation/TestMutations.cs
@@ -0,0 +1,17 @@
+﻿using HotChocolate.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Api.Graphql.Mutation
+{
+    [ExtendObjectType(Name = "Mutation")]
+    public class TestMutations
+    {
+        public string SaveTest()
+        {
+            return "Mutation Working";
+        }
+    }
+}
---
+++ Src/Api/App/Api/Graphql/Queries/TestQueries.cs
@@ -0,0 +1,17 @@
+﻿using HotChocolate.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Api.Graphql.Queries
+{
+    [ExtendObjectType(Name = "Query")]
+    public class TestQueries
+    {
+        public string GetTest()
+        {
+            return "Query Working";
+        }
+    }
+}
