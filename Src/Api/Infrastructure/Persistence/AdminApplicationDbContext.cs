2d4aad61e280e5e2b4604938f40d37a8b7778746
naufil.shaikh <naufil.shaikh@webosphere.in>
2021-02-07 12:17:09 +0530

    Project setup with migrations enable.

---
+++ Src/Api/Infrastructure/Persistence/AdminApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Persistence
+{
+    public class AdminApplicationDbContext : DbContext
+    {
+        public AdminApplicationDbContext(DbContextOptions<AdminApplicationDbContext> options)
+        : base(options)
+        {
+        }
+    }
+}
